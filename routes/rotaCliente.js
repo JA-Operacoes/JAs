17537e10ff20f924a8e32e44d9f311e6c905468f
Marcia <mamalifreitas@gmail.com>
2025-04-22 09:29:45 -0300

    btnPesquisar no Clientes

--- routes/rotaCliente.js
+++ routes/rotaCliente.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69dc40da0c0035c4f62eccc47aab3f27c67193c8e314ece310a7639f97a85b8e
-size 3521
+oid sha256:95a01e85d789f3ced65910d023a20ee135db0974d7548fcf7862a9ac90a0174a
+size 3598
