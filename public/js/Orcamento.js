5eee7743551eee10d28b2390b3a8024f6677a5aa
Marcia <mamalifreitas@gmail.com>
2025-04-24 21:35:38 -0300

    Tabela Orçamento de Itens

--- public/js/Orcamento.js
+++ public/js/Orcamento.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ac000026221838bdefc757f04df05e0397b86d90114fdac8c01cc31c5469e16
-size 19228
+oid sha256:766fbea2e65480ea360e30eb460d807c9d936598d19f7d23b0301058620fea5c
+size 20508
