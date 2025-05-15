6d2007e2059b33f65588df4bade982ec0a2a94f0
Gustavo lima <gustavolimagouveiadefreitas@gmail.com>
2025-05-15 11:44:20 -0300

    criação do Banco e alteração da Rota

--- routes/rotaFuncionario.js
+++ routes/rotaFuncionario.js
@@ -4,63 +4,72 @@ const pool = require("../db/conexaoDB");
 
 // GET todas ou por descrição
 router.get("/", async (req, res) => {
-  const { descFuncao } = req.query;
+  const { nome } = req.query;
 
   try {
-    if (descFuncao) {
+    if (nome) {
       const result = await pool.query(
-        "SELECT * FROM funcionario WHERE nmFuncionario ILIKE $1 LIMIT 1",
-        [descFuncao]
+        "SELECT * FROM funcionarios WHERE nome ILIKE $1 ORDER BY nome ASC LIMIT 1 ",
+        [nome]
       );
       return result.rows.length
         ? res.json(result.rows[0])
-        : res.status(404).json({ message: "Funcionario não encontrada" });
+        : res.status(404).json({ message: "funcionario não encontrada" });
     } else {
-      const result = await pool.query("SELECT * FROM funcionario ORDER BY nmFuncionario ASC");
+      const result = await pool.query("SELECT * FROM funcionarios ORDER BY nome ASC");
       return result.rows.length
         ? res.json(result.rows)
-        : res.status(404).json({ message: "Nenhuma função encontrada" });
+        : res.status(404).json({ message: "Nenhum funcionario encontrado" });
     }
   } catch (error) {
-    console.error("Erro ao buscar funcionario:", error);
-    res.status(500).json({ message: "Erro ao buscar funcionario" });
+    console.error("Erro ao buscar nome :", error);
+    res.status(500).json({ message: "Erro ao buscar nome " });
   }
 });
+
 
 // PUT atualizar
 router.put("/:id", async (req, res) => {
   const id = req.params.id;
-  const { descFuncao, vlrCusto, vlrVenda } = req.body;
+//   const ativo = req.body.ativo;
+ // console.log("Ativo:", ativo); // Log do valor de ativo
+
+  const { areadeatuacao, foto, nome, cpf, rg, contatoPessoal, contatoFamiliar, email, cep, rua, numero, complemento, bairro, cidade, estado, pais  } = req.body;
 
   try {
     const result = await pool.query(
-      `UPDATE Funcionario SET nmFuncionario = $1, apelido = $2, imgFuncionario = $3 WHERE idFuncionario = $4 RETURNING *`,
-      [descFuncao, vlrCusto, vlrVenda, id]
+      `UPDATE funcionarios SET areadeatuacao = $1, foto = &2, nome = &3, cpf = &4, rg = &5, contatoPessoal = &6, contatoFamiliar = &7, email = &8, cep = &9, rua = &10, numero = &11, complemento = &12, bairro = &13, cidade = &14, estado = &15, pais = &16 RETURNING *`,
+      [areadeatuacao, foto, nome, cpf, rg, contatoPessoal, contatoFamiliar, email, cep, rua, numero, complemento, bairro, cidade, estado, pais]
     );
 
     return result.rowCount
-      ? res.json({ message: "Funcionario atualizado com sucesso!", funcao: result.rows[0] })
-      : res.status(404).json({ message: "Funcionario não encontrado para atualizar." });
+      ? res.json({ message: "Funcionario atualizado com sucesso!", funcionarios: result.rows[0] })
+      : res.status(404).json({ message: "Funcionario não encontrada para atualizar." });
   } catch (error) {
-    console.error("Erro ao atualizar funcionario:", error);
-    res.status(500).json({ message: "Erro ao atualizar funcionario." });
+    console.error("Erro ao atualizar funcionarios:", error);
+          // já imprime stack no terminal
+    res.status(500).json({ message: "Erro ao atualizar funcionarios.", detail: error.message });
+    
   }
 });
 
 // POST criar nova função
 router.post("/", async (req, res) => {
-  const { descFuncao, vlrCusto, vlrVenda } = req.body;
+//   const ativo = req.body.ativo === "on" ? true : false;
+  const { areadeatuacao, foto, nome, cpf, rg, contatoPessoal, contatoFamiliar, email, cep, rua, numero, complemento, bairro, cidade, estado, pais} = req.body;
 
   try {
     const result = await pool.query(
-      "INSERT INTO funcionario (nmFuncionario, apelido, imgFuncionario) VALUES ($1, $2, $3) RETURNING *",
-      [descFuncao, vlrCusto, vlrVenda]
+      "areadeatuacao, foto, nome, cpf, rg, contatoPessoal, contatoFamiliar, email, cep, rua, numero, complemento, bairro, cidade, estado, pais) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING *",
+      [areadeatuacao, foto, nome, cpf, rg, contatoPessoal, contatoFamiliar, email, cep, rua, numero, complemento, bairro, cidade, estado, pais]
     );
-    res.json({ mensagem: "Funcionario salva com sucesso!", funcao: result.rows[0] });
+    res.json({ mensagem: "funcionarios salvo com sucesso!", funcionarios: result.rows[0] });
   } catch (error) {
-    console.error("Erro ao salvar funcionario:", error);
-    res.status(500).json({ erro: "Erro ao salvar funcionario" });
+    console.error("Erro ao salvar funcionarios:", error);
+    res.status(500).json({ erro: "Erro ao salvar funcionarios" });
   }
 });
+
+
 
 module.exports = router;
