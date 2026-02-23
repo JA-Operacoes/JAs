341a5626df4c4a8d15df9ffa945252682af57948
Marcia <mamalifreitas@gmail.com>
2026-02-23 16:40:17 -0300

    rotaPagamentos | Alteração da order de visualização dos lançamentos por ordem de descrição

--- routes/rotaPagamento.js
+++ routes/rotaPagamento.js
@@ -71,8 +71,8 @@ router.get("/lancamentos", async (req, res) => {
         // Ajuste os nomes das colunas conforme seu banco (ex: idlancamento, descricao)
         const sql = `SELECT idlancamento, descricao, vlrestimado, vctobase 
                      FROM lancamentos 
-                     WHERE ativo = true 
-                     ORDER BY vctobase ASC`;
+                     WHERE ativo = true                     
+                     ORDER BY descricao ASC`;
         
         const resultado = await pool.query(sql); // Use seu objeto de conexão aqui
         res.json(resultado.rows);
