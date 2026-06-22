cf435010b1f572c7d99ae0900d687db7659c07ef
Marcia <mamalifreitas@gmail.com>
2026-06-22 11:33:00 -0300

    Orcamentos - Correção do Swal que aparecia após fecahr o orçamentos - retirado o window.isCleaning do desinicializarOrcamentosModal deixando apenas como isCleaning

--- public/js/Orcamentos.js
+++ public/js/Orcamentos.js
@@ -4634,7 +4634,7 @@ async function atualizarCampoGeradoAnoPosterior(
 
 function desinicializarOrcamentosModal() {
 
-  window.isCleaning = true;
+  isCleaning = true;
   console.log("🧹 Desinicializando módulo Orcamentos.js");
 
   const selectLocalMontagem = document.getElementById("idMontagem");
